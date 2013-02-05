6d2ff1fafb3a9dcf2bb91043fc19b3d8820ea9f6
Runnan Yang <yang.runnan@gmail.com>
2013-02-05 06:54:51 +0400

    initial substr match + output file to csv

--- NA_match.py
+++ NA_match.py
@@ -4,27 +4,68 @@
 * LINKEDIN ANALYSIS PROJECT II
 * R Yang
 * 
+* Goal: Match names from unify100_1.out with names from NorthAmerica_Companies.csv
+* 
 * University of Chicago 2013
 """
 
 import csv
+import time
 
 def readNA():
     
-    # Load files
-
-    na = []
-    with open('NorthAmerica_Companies.csv', 'rb') as na:
-        naReader = csv.reader(na, delimiter=',')
+    na = {}
+    with open('NorthAmerica_Companies.csv', 'rb') as naFILE:
+        naReader = csv.reader(naFILE, delimiter=',')
         for row in naReader:
-            na.append(row[9])
+            #print row[0], row[9]
+            na[row[9]] = row[0]
+            #na.append(row[9])
 
     u100 = []
-    with open('unify100_1.out', 'rb') as u100:
-        uReader = csv.reader(u100, delimiter='\t')
+    with open('unify100_1.out', 'rb') as u100FILE:
+        uReader = csv.reader(u100FILE, delimiter='\t')
         for row in uReader:
+            #u100[row[1]] = row[0]
             u100.append(row[1])
 
-readNA()
-            
+    return [na, u100]
 
+def main():
+
+    myData = readNA()
+    na = myData[0]
+    u100 = myData[1]
+
+    na_names = na.keys()
+
+    output = []
+
+    count = 0
+    for company in u100:
+        for name in na_names:
+            if name.find(company + " ") == 0:
+                #print company, name
+                #time.sleep(1)
+                count += 1
+                #print count
+
+                out_list = [company, name, na[name]]
+                output.append(out_list)
+    print "count: ", count
+    
+    print "NA: ", len(na)
+    print "u100: ", len(u100)
+
+    with open('na_match.csv', 'wb') as outFILE:
+        outwriter = csv.writer(outFILE, delimiter=',')
+
+        for i in output:
+            outwriter.writerow(i)
+
+    print output[0]
+    print output[1]
+    print output[2]
+
+    
+main()
